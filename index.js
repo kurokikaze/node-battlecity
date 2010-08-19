2938570cdaf9da82416f038c84bbd43779dfc409
Kuroki Kaze <kurokikaze@yandex.ru>
2010-08-19 17:09:27 +0400

    Fixed bricks destroying by bullets

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ var battlecity = {};
 (function(){
 
     battlecity.enemies = {
-     
+        crawler: {
+            speed: 1,
+            mindset: function() {}
+        },
+        runner: {
+            speed: 2,
+            mindset: function() {}
+        },
+        heavy: {
+            speed: 1,
+            mindset: function() {}
+        },
+        hunter: {
+            speed: 1,
+            mindset: function() {}
+        },
+        digger: {
+            speed: 1,
+            mindset: function() {}
+        },
+        vindicator: {
+            speed: 1,
+            mindset: function() {}
+        }
     }
 
     battlecity.map = function(map_array) {
@@ -43,6 +66,13 @@ var battlecity = {};
         ];
 
         var self_map = this;
+
+        var seed_spots = [
+            {'x':1, 'y':1, 'cooldown': 4},
+            {'x':13 * 4, 'y':1, 'cooldown': 4}
+        ]
+
+        var max_enemies = 2;
 
         var tank = function(setx, sety) {
             var x = setx | 0;
@@ -227,7 +257,7 @@ var battlecity = {};
             if (x < 0 || x > (4*13) || y < 0 || y > (4*13)) {
                 return false;
             } else {
-                return map[y, x];
+                return map[y][x];
             }
 
         }
@@ -241,12 +271,18 @@ var battlecity = {};
 
             // Мы попали в объект карты?
             if (self_map.getMapSquare(bullet.x, bullet.y) == '#' ||
-                self_map.getMapSquare(bullet.x, bullet.y) == 'C' ||
                 self_map.getMapSquare(bullet.x, bullet.y + 1) == '#' ||
-                self_map.getMapSquare(bullet.x, bullet.y + 1) == 'C' ||
                 self_map.getMapSquare(bullet.x + 1, bullet.y) == '#' ||
-                self_map.getMapSquare(bullet.x + 1, bullet.y) == 'C' ||
                 self_map.getMapSquare(bullet.x + 1, bullet.y + 1) == '#' ||
+
+                self_map.getMapSquare(bullet.x, bullet.y) == 'E' ||         //
+                self_map.getMapSquare(bullet.x, bullet.y + 1) == 'E' ||     // У кого то проблемы
+                self_map.getMapSquare(bullet.x + 1, bullet.y) == 'E' ||     //
+                self_map.getMapSquare(bullet.x + 1, bullet.y + 1) == 'E' || //
+
+                self_map.getMapSquare(bullet.x, bullet.y) == 'C' ||
+                self_map.getMapSquare(bullet.x, bullet.y + 1) == 'C' ||
+                self_map.getMapSquare(bullet.x + 1, bullet.y) == 'C' ||
                 self_map.getMapSquare(bullet.x + 1, bullet.y + 1) == 'C') {
 
                 console.log('We hit the wall');
@@ -271,10 +307,10 @@ var battlecity = {};
         }
 
         this.addExplosion = function(x, y, direction) {
+
             for (var i = 0; i < 4; i++) {
                 for (var j = 0; j < 4; j++) {
-                    if (this.getMapSquare( x + j, y + i) == '#') {
-                        console.log('clearing ' + (x + j) + ', ' + (y + i));
+                    if (self_map.getMapSquare( x + j, y + i) == '#') {
                         map[y + i][x + j] = ' ';
                     }
                 }
@@ -294,6 +330,8 @@ var battlecity = {};
                 players[player_id].move(this);
                 players[player_id].shoot(this);
             }
+
+            console.log('enemies move');
 
             console.log('bullets');
             // Подвинуть пули (4 раза),..
@@ -373,6 +411,21 @@ var battlecity = {};
                 }
             }
 
+            // Enemy spawning
+            for (var sid in seed_spots) {
+                if (seed_spots[sid].cooldown > 0) {
+                    seed_spots[sid].cooldown -= 1;
+                } else {
+                    // How much enemies are there?
+                    if (enemies.length < max_enemies) {
+                        seed_spots[sid].cooldown = 10;
+                        enemies.push({'x':seed_spots[sid].x,'y':seed_spots[sid].y, 'type':'crawler'});
+                        console.log('Crawler spawned at ' + seed_spots[sid].x + ', ' + seed_spots[sid].y);
+                    }
+
+                }
+            }
+
             console.log('end');
             // Конец хода
             turn_number++;
@@ -394,44 +447,28 @@ var battlecity = {};
                 map_sub[rid] = map[rid].slice(0);
             }
 
-//            var map_sub = map.slice(0);
-
             for (var row in map_sub) {
 
                 for (var pid in players) {
                     if (players[pid].position()[1] == row) {
-                        //if (players[pid].position()[1] > map[wor]
-                        //console.log('Player found on position ' + players[pid].position()[1] + ' of ' + map_sub[row].length);
-                        //for (var i = 0; i <= 4; i++) {
-                        //    for (var j = 0; j <= 4; j++) {
-                                map_sub[parseInt(row)][players[pid].position()[0]] = 'T';
-                                map_sub[parseInt(row) + 3][players[pid].position()[0]] = 'T';
-                                map_sub[parseInt(row)][players[pid].position()[0] + 3] = 'T';
-                                map_sub[parseInt(row) + 3][players[pid].position()[0] +3] = 'T';
-                        //    }
-                        //}
+                        map_sub[parseInt(row)][players[pid].position()[0]] = 'T';
+                        map_sub[parseInt(row) + 3][players[pid].position()[0]] = 'T';
+                        map_sub[parseInt(row)][players[pid].position()[0] + 3] = 'T';
+                        map_sub[parseInt(row) + 3][players[pid].position()[0] +3] = 'T';
                     }
                 }
 
                 for (var eid in explosions) {
                     if (explosions[eid].y == row) {
-                        //if (players[pid].position()[1] > map[wor]
-                        //console.log('Player found on position ' + players[pid].position()[1] + ' of ' + map_sub[row].length);
-                        //for (var i = 0; i <= 4; i++) {
-                        //    for (var j = 0; j <= 4; j++) {
-                                map_sub[parseInt(row)][explosions[eid].x] = '!';
-                                map_sub[parseInt(row) + 3][explosions[eid].x] = '!';
-                                map_sub[parseInt(row)][explosions[eid].x + 3] = '!';
-                                map_sub[parseInt(row) + 3][explosions[eid].x +3] = '!';
-                        //    }
-                        //}
+                        map_sub[parseInt(row)][explosions[eid].x] = '!';
+                        map_sub[parseInt(row) + 3][explosions[eid].x] = '!';
+                        map_sub[parseInt(row)][explosions[eid].x + 3] = '!';
+                        map_sub[parseInt(row) + 3][explosions[eid].x +3] = '!';
                     }
                 }
 
                 for (var bid in bullets) {
                     if (bullets[bid].y == row) {
-
-                        //console.log('Bullet at ' + bullets[bid].y + ', ' + bullets[bid].x);
 
                         map_sub[bullets[bid].y][bullets[bid].x] = '*';
                         map_sub[bullets[bid].y + 1][bullets[bid].x] = '*';
--- test.js
+++ test.js
@@ -11,26 +11,6 @@ sys.puts('');
 setInterval(function() {
     stage.progress();
     stage.showmap();
-}, 1000);
+}, 500);
 
 
-    battlecity.enemies = {
-        crawler: {
-            speed: 1;
-        },
-        runner: {
-            speed: 2;
-        },
-        heavy: {
-            speed: 1;
-        },
-        hunter: {
-            speed: 1;
-        },
-        digger: {
-            speed: 1;
-        },
-        vindicator: {
-            speed: 1;
-        }
-    }
